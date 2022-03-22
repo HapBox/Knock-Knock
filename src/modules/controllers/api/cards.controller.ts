ccc8c6d59a76d1e4e4cb99988906f3f23865f50c
Никита Матяш <ndmatyash@gmail.com>
2022-03-22 22:22:17 +0300

    add card service

--- src/modules/controllers/api/cards.controller.ts
+++ src/modules/controllers/api/cards.controller.ts
@@ -1,35 +1,39 @@
 import { NextFunction, Response } from 'express';
 import { ApiController, DELETE, GET, PATCH, POST } from '../../../core/api-decorators';
+import { requireToken } from '../../../middlewares/require-token';
+import { dtoValidator } from '../../../middlewares/validate';
 import BaseRequest from '../../base/base.request';
+import { CardCreateDto } from '../../dto/card-create.dto';
+import ApiCardsService from '../../services/api/api-cards.service';
 
 @ApiController('/api/cards')
 class Controller {
   @POST('/', {
     summary: 'Создание новой карты пользователя',
+    handlers: [requireToken, dtoValidator(CardCreateDto)],
   })
   async createCard(req: BaseRequest, res: Response, next: NextFunction) {
-    res.json({ message: 'ok' });
+    const dto = { ...req.body, userId: req.userId };
+    const card = await ApiCardsService.createCard(dto);
+    res.json(card);
   }
 
   @GET('/', {
     summary: 'Получение списка карт',
+    handlers: [requireToken],
   })
   async getCards(req: BaseRequest, res: Response, next: NextFunction) {
-    res.json({ message: 'ok' });
-  }
-
-  @PATCH('/:id/default', {
-    summary: 'Делает карту избранной',
-  })
-  async updateCard(req: BaseRequest, res: Response, next: NextFunction) {
-    res.json({ message: 'ok' });
+    const cardList = await ApiCardsService.getCards(req.userId);
+    res.json(cardList);
   }
 
   @DELETE('/:id', {
     summary: 'Удаляет карту',
+    handlers: [requireToken],
   })
   async deleteCard(req: BaseRequest, res: Response, next: NextFunction) {
-    res.json({ message: 'ok' });
+    await ApiCardsService.deleteCard(req.params.id, req.userId);
+    res.json({ message: 'succesfull' });
   }
 }
 
---
+++ src/modules/services/api/api-cards.service.ts
@@ -0,0 +1,37 @@
+import Card from '../../../database/models/final/card.model';
+import { throwError } from '../../../utils/http-exception';
+import { CardCreateDto } from '../../dto/card-create.dto';
+
+export default class ApiCardsService {
+  static async getCards(userId: string) {
+    const cardList = await Card.findAll({
+      where: {
+        userId: userId,
+        isExist: true,
+      },
+    });
+    return cardList;
+  }
+
+  static async createCard(dto: CardCreateDto) {
+    const card = await Card.create(dto);
+    return card;
+  }
+
+  static async deleteCard(cardId: string, userId: string) {
+    const card = await Card.findOne({
+      where: {
+        userId: userId,
+        id: cardId,
+        isExist: true,
+      },
+    });
+    if (!card) {
+      throwError({
+        statusCode: 404,
+        message: 'Not found.',
+      });
+    }
+    card.update({isExist: false});
+  }
+}
